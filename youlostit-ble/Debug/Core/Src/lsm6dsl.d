fe155c68dd65c97b07dbc1e62aad4dbf74e82a24
kate-romero <kgromero@ucsd.edu>
2025-02-28 17:00:33 -0800

    syntax error

---
+++ youlostit-ble/Debug/Core/Src/lsm6dsl.d
@@ -0,0 +1,15 @@
+Core/Src/lsm6dsl.o: ../Core/Src/lsm6dsl.c \
+ ../Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l475xx.h \
+ ../Drivers/CMSIS/Include/core_cm4.h \
+ ../Drivers/CMSIS/Include/cmsis_version.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../Drivers/CMSIS/Include/mpu_armv7.h \
+ ../Drivers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h
+../Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l475xx.h:
+../Drivers/CMSIS/Include/core_cm4.h:
+../Drivers/CMSIS/Include/cmsis_version.h:
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+../Drivers/CMSIS/Include/cmsis_gcc.h:
+../Drivers/CMSIS/Include/mpu_armv7.h:
+../Drivers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h:
