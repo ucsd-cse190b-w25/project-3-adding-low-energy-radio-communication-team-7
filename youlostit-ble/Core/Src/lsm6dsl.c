3d04eb5b1c421746daf5caa095db5d7dcf1176fa
kate-romero <kgromero@ucsd.edu>
2025-03-12 19:08:22 -0700

    accelerometer side of the interrupt done. need to configure cpu gpio as interrupt and handle it

--- youlostit-ble/Core/Src/lsm6dsl.c
+++ youlostit-ble/Core/Src/lsm6dsl.c
@@ -9,21 +9,31 @@
 void lsm6dsl_init()
 {
 
-    uint8_t ctrl1_xl[2] = {0x10, 0x40};		// 104 Hz for timer
-    uint8_t int1_ctrl[2] = {0x0D, 0x01};	// TODO: change interrupt config
+    uint8_t ctrl1_xl[2] = {0x10, 0x4c};		// 104 Hz for timer, +-8g
+//    uint8_t int1_ctrl[2] = {0x0D, 0x01};	// TODO: change interrupt config
 
     i2c_transaction(0x6a, 0, ctrl1_xl, 2); // write CTRL1_XL = 40h
 
-    i2c_transaction(0x6a, 0, int1_ctrl, 2); // write INT1_CTRL = 01h
+//    i2c_transaction(0x6a, 0, int1_ctrl, 2); // write INT1_CTRL = 01h
 };
 
-void lsm6dsl_setup_interrupts()
+void lsm6dsl_setup_interrupts()	//TODO: i2c addresses
 {
-	// enable interrupts
-	// configure inactive settings
+	//turn on gyroscope
+	uint8_t ctrl2_g[2] = {0x11, 0x40};
+	i2c_transaction(0x6a, 0, ctrl2_g, 2);
 	// set inactivity time threshold
+	uint8_t wake_up_dur[2] = {0x5c, 0x0c};
+	i2c_transaction(0x6a, 0, wake_up_dur, 2);	// inactive dur thresh = 12 (60 seconds)
 	// set gyroscope threshold
+	uint8_t wake_up_ths[2] = {0x5b, 0x14};
+	isc_transaction(0x6a, 0, wake_up_ths, 2);	// +-5g
+	// enable interrupts
+	uint8_t tap_cfg[2] = {0x58, 0xe0};
+	i2c_transaction(0x6a, 0, tap_cfg, 2);		// gyro to power down mode {?}
 	// drive interrupts to pin
+	uint8_t md1_cfg[2] = {0x5e, 0x80};
+	i2c_transaction(0x6a, 0, md1_cfg, 2);
 
 	// change ODR_XL
 }
